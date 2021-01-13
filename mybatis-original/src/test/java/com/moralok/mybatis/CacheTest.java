abddd0b7b984f6ab141bde2c92b48f1b0a506c7b
moralok <moralok@163.com>
2021-01-13 14:16:52 +0800

    Mybatis-缓存-介绍和一级缓存的4种失效情况

--- mybatis-original/src/test/java/com/moralok/mybatis/CacheTest.java
+++ mybatis-original/src/test/java/com/moralok/mybatis/CacheTest.java
@@ -1,5 +1,6 @@
 package com.moralok.mybatis;
 
+import com.moralok.mybatis.bean.Department;
 import com.moralok.mybatis.bean.Employee;
 import com.moralok.mybatis.mapper.EmployeeMapper;
 import org.apache.ibatis.io.Resources;
@@ -26,9 +27,35 @@ public class CacheTest {
             Employee employee = mapper.getEmployeeById(1);
             System.out.println(employee);
             // 不会执行SQL
+            // 如果执行过修改，缓存失效
+            // 如果主动清理缓存，缓存失效
+            // mapper.addEmployee(new Employee(null, "Jerry", true, "Jerry@gmail.com", new Department(2)));
+            sqlSession.clearCache();
             Employee employee2 = mapper.getEmployeeById(1);
             System.out.println(employee);
-            Assertions.assertSame(employee, employee2);
+            System.out.println(employee == employee2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void firstLevelCacheInvalidTest() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+        try (SqlSession sqlSession = sqlSessionFactory.openSession(); SqlSession sqlSession2 = sqlSessionFactory.openSession()) {
+
+            EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+            Employee employee = mapper.getEmployeeById(1);
+            System.out.println(employee);
+            // 1. 不同sqlSession将重新查询
+            // 2. 不同查询条件
+            // 3. 两次中间执行过增删改
+            // 4. 手动清理缓存
+            EmployeeMapper mapper2 = sqlSession2.getMapper(EmployeeMapper.class);
+            Employee employee2 = mapper2.getEmployeeById(1);
+            System.out.println(employee2);
+            Assertions.assertNotSame(employee, employee2);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
