67060d7668753f609adc23bb33b0e1775b1fa841
moralok <moralok@163.com>
2021-01-23 17:05:43 +0800

    Head First Servlet&JSP - ch2 Servlet使用

---
+++ simple-web/src/main/java/com/moralok/servlet/servlet/Ch1Servlet.java
@@ -0,0 +1,29 @@
+package com.moralok.servlet.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * @author moralok
+ * @since 2021/1/23
+ */
+public class Ch1Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        Date today = new Date();
+        writer.println("<html>" +
+                "<body>" +
+                "<h1>Chapter1 Servlet</h1>" +
+                "<br>" +
+                today +
+                "</body>" +
+                "</html>");
+    }
+}
