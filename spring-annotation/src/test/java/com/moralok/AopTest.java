a42f5e1dc559e09162654397f4009d3e6977666a
moralok <moralok@163.com>
2022-02-22 00:27:21 +0800

    refactor(docs): 复习AOP以及AnnotationAwareAspectJAutoProxyCreator的创建
    复习AOP以及AnnotationAwareAspectJAutoProxyCreator的创建

--- spring-annotation/src/test/java/com/moralok/AopTest.java
+++ spring-annotation/src/test/java/com/moralok/AopTest.java
@@ -6,6 +6,22 @@ import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
+ * registerBeanPostProcessors(beanFactory)注册BeanPostProcessor
+ *     先获取ioc容器中已经定义的后置处理器
+ *     加别的后置处理器
+ *     按PriorityOrdered、Ordered和剩余的排序且按顺序注册（实际上就是创建实例）
+ *         创建实例
+ *         populateBean属性赋值
+ *         初始化Bean initializeBean
+ *             invokeAwareMethods
+ *             applyBeanPostProcessorsBeforeInitialization
+ *             invokeInitMethods
+ *             applyBeanPostProcessorsAfterInitialization
+ *         AnnotationAwareAspectJAutoProxyCreator创建成功，调用了initBeanFactory（里面包装了一下BeanFactory）
+ *     BeanFactory.addBeanPostProcessor 后面Bean的创建就会被拦截了
+ *
+ * AnnotationAwareAspectJAutoProxyCreator是InstantiationAwareBeanPostProcessor类型
+ *
  * @author moralok
  * @since 2020/12/19
  */
